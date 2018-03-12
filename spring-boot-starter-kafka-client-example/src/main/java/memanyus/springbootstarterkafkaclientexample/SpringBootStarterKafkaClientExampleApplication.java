7e7d90f9d63914ff3e70aecadc3deaa825dc063a
Byron Ellis <byron.ellis@gmail.com>
2018-03-11 19:25:36 -0700

    Initial skeleton commit

---
+++ spring-boot-starter-kafka-client-example/src/main/java/memanyus/springbootstarterkafkaclientexample/SpringBootStarterKafkaClientExampleApplication.java
@@ -0,0 +1,12 @@
+package memanyus.springbootstarterkafkaclientexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterKafkaClientExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStarterKafkaClientExampleApplication.class, args);
+	}
+}
